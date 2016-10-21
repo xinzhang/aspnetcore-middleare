3096641b521212ea49965030b652cb6c3b017559
xin.zhang <xin_zhang_2000@yahoo.com>
2016-10-21 16:15:45 +1100

    final

---
+++ MyMiddleware.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace WebApplication2
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class MyMiddleware
+    {
+        private readonly RequestDelegate _next;
+        ILoggerFactory _loggerFactory;
+        IOptions<MyMiddlewareOptions> _options;
+
+        public MyMiddleware(RequestDelegate next, ILoggerFactory factory, IOptions<MyMiddlewareOptions> options)
+        {
+            _next = next;
+            _loggerFactory = factory;
+            _options = options;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            _loggerFactory.AddConsole();
+            var logger = _loggerFactory.CreateLogger("My Logger");
+            logger.LogInformation("My middleware invoked");
+
+            await httpContext.Response.WriteAsync("This is my middleware</br> and option is " + _options.Value.OptionOne);
+            await _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class MyMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMyMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<MyMiddleware>();
+        }
+    }
+
+    public class MyMiddlewareOptions
+    {
+        public string OptionOne { get; set; }
+    }
+
+}
---
+++ Startup.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Routing;
+
+namespace WebApplication2
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                   .SetBasePath(env.ContentRootPath)
+                   .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOptions();
+
+            services.AddCors();
+
+            services.AddRouting();
+
+            var myoption = Configuration.GetSection("MyMiddlewareOptionsSection");
+            services.Configure<MyMiddlewareOptions>(o => o.OptionOne = myoption["OptionOne"]);            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            //app.MapWhen()
+
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello first component.<br/>");
+            //    await next.Invoke();
+            //    await context.Response.WriteAsync("Hello first by the way.<br/>");
+            //});
+
+            app.UseStaticFiles();
+
+            app.UseCors( (builder) =>
+            {
+                builder.AllowAnyOrigin();
+            });
+
+            var routeBuilder = new RouteBuilder(app);
+            routeBuilder.MapGet("greeting/{name}", (appbuilder) =>
+            {
+                appbuilder.Run(async (context) =>
+                {
+                    var name = context.GetRouteValue("name");
+                   await context.Response.WriteAsync($"Greeting {name} <br/>");
+                });
+            });
+
+            app.UseRouter(routeBuilder.Build());
+
+            app.UseMyMiddleware();
+
+            //app.Map("/mybooks", (appBuilder) =>
+            //{
+            //    appBuilder.Use(async (context, next) =>
+            //   {
+            //       await context.Response.WriteAsync("my books <br/>");
+            //       await next.Invoke();
+            //   });
+            //});
+
+            //app.MapWhen(context => context.Request.Query.ContainsKey("key"), (appBuilder) =>
+            //{
+            //    appBuilder.Run(async (context) =>
+            //    {
+            //        await context.Response.WriteAsync("mapped the item <br/>");
+            //    });
+            //});
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World in the last pipeline. <br/>");
+            //});
+
+        }
+    }
+}
